44e2858fdd2b8643e095db94c5771a0ec62fe448
Asjad <chasjad@vijump.com>
2022-08-17 18:34:54 +0500

    tests passed

--- src/App.js
+++ src/App.js
@@ -1,8 +1,7 @@
 import React from 'react';
 import './App.css';
-import ResidentsList from './Components/ResidentsList';
 import Search from './Components/Search';
-import Error from './Components/Error';
+
 import 'h8k-components';
 
 const title = "Hacker Dormitory";
@@ -12,8 +11,6 @@ function App() {
         <h8k-navbar header={title}></h8k-navbar>
       <div className="layout-column justify-content-center align-items-center w-50 mx-auto">
         <Search />
-        <Error/>
-        <ResidentsList/>
       </div>
     </div>
   );
--- src/Components/Search.js
+++ src/Components/Search.js
@@ -1,32 +1,65 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { STUDENTS } from '../studentsList'
+import Error from './Error';
+import ResidentsList from './ResidentsList';
 
-// `joiningDate` && `validityDate` format "yyyy-mm-dd"
 
-function checkValidity(joiningDate, validityDate) {
-	const now = new Date();
-	const today = new Date(now.getFullYear(), now.getMonth(), now.getDate());
-	const [year, month, day] = joiningDate.split('-');
-	const [yyyy, mm, dd] = validityDate.split('-');
-	const maxValid = new Date(yyyy, mm - 1, dd);
-	const selected = new Date(year, month - 1, day);
-	return (maxValid >= selected) && (maxValid >= today);
+function checkDateValidity(joiningDate, validityDate) {
+	var joiningDate = Date.parse(joiningDate);
+	var validityDate = Date.parse(validityDate);
+	return (joiningDate > validityDate) ? false : true;
+}
+
+function getNameIfPresent(name) {
+	let studentPresent = STUDENTS.find((student => student.name.toLowerCase() === name.toLowerCase()));
+	return studentPresent;
 }
 
 function Search() {
+	const [studentName, setStudentName] = useState('');
+	const [joiningDate, setJoiningDate] = useState('');
+	const [errorMessage, setErrorMessage] = useState('');
+	const [residentList, setResidentList] = useState([]);
+
+	const addStudent = () => {
+		const student  = getNameIfPresent(studentName);
+		if (!student) {
+			setErrorMessage(`Sorry, ${studentName} is not a verified student!`);
+		}
+		else if (!checkDateValidity(joiningDate,student.validityDate)) {
+			setErrorMessage(`Sorry, ${studentName}'s validity has Expired!`);
+		}
+		else {
+			setResidentList([...residentList, studentName]);
+			setStudentName('');
+			setJoiningDate('');
+			setErrorMessage('');
+		}
+	}
 	return (
-		<div className="my-50 layout-row align-items-end justify-content-end">
-			<label htmlFor="studentName">Student Name:
-				<div>
-					<input id="studentName" data-testid="studentName" type="text" className="mr-30 mt-10"/>
-				</div>
-			</label>
-			<label htmlFor="joiningDate">Joining Date:
-				<div>
-					<input id="joiningDate" data-testid="joiningDate" type="date" className="mr-30 mt-10"/>
-				</div>
-			</label>
-			<button type="button" data-testid="addBtn" className="small mb-0">Add</button>
+		<div className="layout-column justify-content-center align-items-center w-50 mx-auto">
+			<div className="my-50 layout-row align-items-end justify-content-end">
+
+				<label htmlFor="studentName">Student Name:
+					<div>
+						<input id="studentName" data-testid="studentName" value={studentName}
+						 onChange={e => { setStudentName(e.target.value) }} type="text" className="mr-30 mt-10" />
+					</div>
+				</label>
+				<label htmlFor="joiningDate">Joining Date:
+					<div>
+						<input id="joiningDate" data-testid="joiningDate" value={joiningDate}
+						onChange={e => { setJoiningDate(e.target.value) }} type="date" className="mr-30 mt-10" />
+					</div>
+				</label>
+				<button type="button" onClick={() => { addStudent() }} data-testid="addBtn" className="small mb-0">Add</button>
+
+			</div>
+			{errorMessage ? <Error  msg={errorMessage}/> : ''}
+			<ResidentsList residentList={residentList}/>
 		</div>
+
+
 	);
 }
 
